b75047858078463177acb1dfd7e56498b7e0ea01
yanyuzuixin <yanyuzuixin@163.com>
2020-12-28 23:53:15 +0800

    单一职责原则

---
+++ src/com/hjr/principle/singleresponsibility/SingleResponsibility3.java
@@ -0,0 +1,28 @@
+package com.hjr.principle.singleresponsibility;
+
+public class SingleResponsibility3 {
+    public static void main(String[] args) {
+        Vehicle2 vehicle2 = new Vehicle2();
+        vehicle2.run("汽车");
+        vehicle2.runWater("轮船");
+        vehicle2.runAir("飞机");
+    }
+}
+
+/**
+ * 方案3分析
+ * 1.这种方法没有对原来的类做大的修改，只是增加方法
+ * 2.这里虽然没有在类这个级别上遵守单一职责原则，但是在方法级别是遵守单一职责原则的
+ */
+
+class Vehicle2 {
+    public void run(String vehicle) {
+        System.out.println(vehicle + " 在公路上运行");
+    }
+    public void runAir(String vehicle) {
+        System.out.println(vehicle + " 在天空中运行");
+    }
+    public void runWater(String vehicle) {
+        System.out.println(vehicle + " 在水中运行");
+    }
+}
